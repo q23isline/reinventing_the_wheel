acfe595d8bad7a4df179ac5d560613456a80bbe7
rowpure <rowpure@gmail.com>
2021-04-25 18:47:41 +0900

    ユーザー詳細APIのモックAPI作成

--- config/routes.php
+++ config/routes.php
@@ -100,4 +100,5 @@ $routes->scope('/api/v1', ['prefix' => 'Api/V1'], function (RouteBuilder $builde
     $builder->setExtensions(['json']);
 
     $builder->connect('/users', ['controller' => 'Users', 'action' => 'index'])->setMethods(['GET']);
+    $builder->connect('/users/:userId', ['controller' => 'Users', 'action' => 'view'])->setMethods(['GET']);
 });
--- src/Controller/Api/V1/UsersController.php
+++ src/Controller/Api/V1/UsersController.php
@@ -37,4 +37,32 @@ class UsersController extends AppController
         $this->viewBuilder()->setClassName('Json')
             ->setOption('serialize', ['data']);
     }
+
+    /**
+     * View method
+     *
+     * @return \Cake\Http\Response|null|void Renders json
+     */
+    public function view()
+    {
+        $userId = $this->request->getParam('userId');
+
+        // TODO: モックAPIを修正する
+        $data = [
+            'data' => [
+                'id' => 1,
+                'loginId' => 'saitou',
+                'roleName' => 'viewer',
+                'firstName' => '斉藤',
+                'lastName' => '太郎',
+                'created' => '2019-08-24T14:15:22Z',
+                'modified' => '2019-08-24T14:15:22Z',
+            ],
+        ];
+
+        $this->set($data);
+        // .jsonなしでもOKとする
+        $this->viewBuilder()->setClassName('Json')
+            ->setOption('serialize', ['data']);
+    }
 }
