c35a3fb33d13f4919121ec2826f212ac74179d3a
q23isline <rowpure@gmail.com>
2022-02-13 14:31:09 +0900

    Authenticationプラグイン追加によるapplicationのテスト変更

--- tests/TestCase/ApplicationTest.php
+++ tests/TestCase/ApplicationTest.php
@@ -40,11 +40,12 @@ class ApplicationTest extends IntegrationTestCase
         $app->bootstrap();
         $plugins = $app->getPlugins();
 
-        $this->assertCount(4, $plugins);
+        $this->assertCount(5, $plugins);
         $this->assertSame('Cake/Repl', $plugins->get('Cake/Repl')->getName());
         $this->assertSame('Bake', $plugins->get('Bake')->getName());
         $this->assertSame('DebugKit', $plugins->get('DebugKit')->getName());
         $this->assertSame('Migrations', $plugins->get('Migrations')->getName());
+        $this->assertSame('Authentication', $plugins->get('Authentication')->getName());
     }
 
     /**
