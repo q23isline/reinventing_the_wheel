e138a72d1f96f04e687436f5bd330ed42fe0ca06
q23isline <rowpure@gmail.com>
2022-06-19 14:11:44 +0900

    ユーザー詳細で使用しないリレーションモデルを削除

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -50,7 +50,7 @@ class UsersController extends AppController
     public function view($id = null)
     {
         $user = $this->Users->get($id, [
-            'contain' => ['Profiles'],
+            'contain' => [],
         ]);
 
         $this->set(compact('user'));
