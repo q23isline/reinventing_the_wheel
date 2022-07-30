fddd9e5395da06ec02bd8338f94548d814059919
q23isline <rowpure@gmail.com>
2022-07-30 18:04:40 +0900

    ユーザーのscaffoldしたソースにテスト作成

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -67,6 +67,10 @@ class UsersController extends AppController
         $user = $this->Users->newEmptyEntity();
         if ($this->request->is('post')) {
             $user = $this->Users->patchEntity($user, $this->request->getData());
+
+            // SQLite だと自動で ID に UUID を設定してくれないため、手動で設定する
+            $user->id = Text::uuid();
+
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
 
