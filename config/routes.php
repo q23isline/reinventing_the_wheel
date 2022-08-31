05c240aa489a5a5eacce05230d739a835ffb6b86
q23isline <rowpure@gmail.com>
2022-08-31 23:59:39 +0900

    PHPUnitエラー修正

--- config/routes.php
+++ config/routes.php
@@ -72,11 +72,11 @@ return static function (RouteBuilder $routes) {
          * You can remove these routes once you've connected the
          * routes you want in your application.
          */
+        $builder->fallbacks();
+
         if (Configure::read('debug')) {
             $builder->connect('/{controller}/{action}/*', ['plugin' => 'DebugKit']);
         }
-
-        $builder->fallbacks();
     });
 
     /*
